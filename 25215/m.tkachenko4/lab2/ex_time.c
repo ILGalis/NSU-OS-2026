a37b7fd0c831b90d09504a29c0713f952590990f
Michael Tkachenko <m.tkachenko4@g.nsu.ru>
2026-09-15 14:53:13 +0700

    lab2: fix ex_time (PST output)

--- 25215/m.tkachenko4/lab2/ex_time.c
+++ 25215/m.tkachenko4/lab2/ex_time.c
@@ -15,7 +15,6 @@ int main()
     (void) time(&now);
     printf("%s", ctime(&now));
     sp = localtime(&now);
-    if (!sp) { perror("localtime"); return 1; }
     printf("%d/%d/%02d %d:%02d %s\n",
            sp->tm_mon + 1, sp->tm_mday,
            sp->tm_year, sp->tm_hour,
